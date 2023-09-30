7830c769f8576da96347d4156a7896641ee797e2
Prakash Raghothamachar <prakashr@itprakashr.city-furniture.local>
2023-09-30 14:30:46 -0400

    updates.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,8 +5,6 @@ import ThemeProvider from '@mui/material/styles/ThemeProvider'
 import { theme } from '@/styles'
 import CompanyForm from '@/components/CompanyForm'
 import Grid from '@mui/material/Grid'
-import Entry from '@/components/Entry'
-import Recommendation from '@/components/Recommendation'
 
 export default function Home() {
   return (
@@ -16,7 +14,7 @@ export default function Home() {
         <Box component='main' sx={{ flexGrow: 1, p: 15 }}>
           <Grid container>
             <Grid item xs={12}>
-              <Recommendation></Recommendation>
+              <CompanyForm></CompanyForm>
             </Grid>
           </Grid>
         </Box>
