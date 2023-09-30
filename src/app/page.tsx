b82e3666551b6bc39f3cae5d5786d6f3eb927a27
Prakash Raghothamachar <prakashr@itprakashr.city-furniture.local>
2023-09-30 18:10:23 -0400

    flip implementation with background image.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -49,6 +49,12 @@ export default function Home() {
           </div>
           <Grid container>
             <Grid item xs={12}>
+              <CompanyForm></CompanyForm>
+              <br></br>
+            </Grid>
+          </Grid>
+          <Grid container>
+            <Grid item xs={12}>
               <WhyPrescreening></WhyPrescreening>
             </Grid>
             <br></br>
