2e1d717a586ccf67eba40518b78fa5f2f7dbcb45
Shashank Kumar Mishra <Shashank@CITY-FURNITURE.com>
2023-09-30 19:59:18 -0400

    theme color and name change

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,12 +1,21 @@
 import { createTheme } from '@mui/material/styles'
 
 export const theme = createTheme({
-  typography: {
-    h3: {
-      fontFamily: 'Poppins'
+  // typography: {
+  //   h3: {
+  //     fontFamily: 'Poppins'
+  //   },
+  //   allVariants: {
+  //     fontFamily: 'Source Sans 3'
+  //   }
+  // } 
+  palette: {
+    primary: {
+      main: '#ff0000', // Replace with your desired primary color
     },
-    allVariants: {
-      fontFamily: 'Source Sans 3'
-    }
-  }
+    secondary: {
+      main: '#2196f3', // Replace with your desired secondary color
+    },
+    // You can override other colors as well, such as error, warning, success, etc.
+  },
 })
