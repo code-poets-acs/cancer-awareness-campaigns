4b8ae664e243ec4adab1041131d8064a8ed25047
Ryan Young <ryany@city-furniture.com>
2023-09-30 20:40:50 -0400

    styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,25 +5,19 @@ import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import { Grid, Typography } from '@mui/material'
 
-
 export default function Header() {
-  const gridStyle = {
-    wid: '15px'
-  }
   return (
     <AppBar position='fixed' sx={{ zIndex: theme => theme.zIndex.drawer + 1 }}>
       <Toolbar>
-        {/* <Typography variant='h4' component='div' sx={{ flexGrow: 1, padding: 1 }}>
-          American Cancer Society
-          <img src='acs-logo.svg'
-          ></img>
-        </Typography> */}
-
-        <Grid item xs={3}>
-          <b> {`American Cancer Society`}</b>
-        </Grid>
-        <Grid item xs={3} style={gridStyle}>
-          <img src=''></img>
+        <Grid container justifyContent='space-between' alignContent='center'>
+          <Grid item xs={3}>
+            <Typography variant='h5' component='div' sx={{ flexGrow: 1, padding: 1 }}>
+              American Cancer Society
+            </Typography>
+          </Grid>
+          <Grid item xs={0.5}>
+            <img style={{ width: 80 }} src='acs-logo.png'></img>
+          </Grid>
         </Grid>
       </Toolbar>
     </AppBar>
